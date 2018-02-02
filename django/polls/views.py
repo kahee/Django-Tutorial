e5fd2bea0f21c92f15e230a71d3a198cf9c8be59
kahee <yuygh131@gmail.com>
2018-02-02 15:39:25 +0900

    try-except 구문 대신 get_object_or_404 shortcut function을 사용

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -19,13 +19,18 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
+    # try:
+    #
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    # context = {
+    # 'question': question
+    # }
+    # return render(request, 'polls/detail.html', context)
 
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
     context = {
-    'question': question
+        'question': question
     }
     return render(request, 'polls/detail.html', context)
 
