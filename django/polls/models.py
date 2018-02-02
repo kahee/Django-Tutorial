6dc10acc12d241992d0af116d71ebca462d3c2b9
kahee <yuygh131@gmail.com>
2018-02-02 14:19:20 +0900

    Choice, Question admin에 추가

--- django/polls/models.py
+++ django/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -11,8 +14,17 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        """
+        이 Question의 게시일자가 현재시각에서 1일 전보다 이후인지
+        ( Question을 게시한지 1일이 지나지 않았는지)
+        :return:
+        """
+        return self.pub_date >= timezone.now() - datetime.timedlta(days=1)
+
 
 class Choice(models.Model):
+    # 다대일 관계
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
