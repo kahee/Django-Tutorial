91d67ea91c9f5f5822a8969fbff0d3d2f31af9e9
kahee <yuygh131@gmail.com>
2018-02-02 15:47:41 +0900

    detail로 가는  url링크를 동적으로 바꿈

--- django/polls/views.py
+++ django/polls/views.py
@@ -30,7 +30,7 @@ def detail(request, question_id):
 
     question = get_object_or_404(Question, pk=question_id)
     context = {
-        'question': question
+        'question': question,
     }
     return render(request, 'polls/detail.html', context)
 
