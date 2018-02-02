512db35cf45f49fb49d2fab0cffb71a71b06195a
kahee <yuygh131@gmail.com>
2018-02-02 14:34:48 +0900

    urls.py  수정

--- django/polls/views.py
+++ django/polls/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def results(request, question_id):
+    response = "Your,re looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, qestion_id):
+    return HttpResponse("You're voting on question %s." %qestion_id)
