813a81b22d505b2935ff75507bdcb0e1b2f3f5cf
kahee <yuygh131@gmail.com>
2018-02-02 15:28:41 +0900

    index view가 템플릿을 사용하도록 변경, detail view에 Choice 목록을 출력

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 
-    # # 정규표현식으로 $을 안붙이면 밑에 애들은 인식못함
-    # re_path(r'^$', views.index, name='index'),
-    # re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    # re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
-
+    # re_path를 사용해서 정규표현식으로 구현해보세요
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 ]
--- django/polls/views.py
+++ django/polls/views.py
@@ -1,12 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 가장 최근 발행된 최대 4개의  Question 목록
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 쉼표 단위로 구분된 Question 목록의 각 항목의 question_text로 만들어진 문자열
+    # output = ', '.join([q.question_text for q in lastest_question_list])
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # template = loader.get_template('poll/index.html')
+    # return HttpResponse(template.render(context,request))
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question
+    }
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', context)
+
 
 def results(request, question_id):
     response = "Your,re looking at the results of question %s."
     return HttpResponse(response % question_id)
 
-def vote(request, qestion_id):
-    return HttpResponse("You're voting on question %s." %qestion_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
